0c83737e759b9f444ebd5116d38b4c0d4ad5100f
levigem <levi@trov.com>
2017-05-01 16:05:47 -0700

    working on posting to database

--- jakulebe_code/routes/gameroom.js
+++ jakulebe_code/routes/gameroom.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var {database} = require('../database/database');
 
 router.get('/', function(req, res) {
   res.render('gameroom');
+});
+
+router.post('/createGameRoom', function(req, res, next)
+{
+  let gameRoomName = req.body.gameRoomName;
+  const numberOfPlayers = 4;
+  var insertQuery = `INSERT INTO Games (gameRoomName, max_players) VALUES ($1, $2)`;
+  client.query(insertQuery, [gameRoomName, numberOfPlayers]);
 });
 
 module.exports = router;
