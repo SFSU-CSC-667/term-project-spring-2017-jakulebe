74bbf7926b80c2cee6c34cf25b6f0ad1a385fb6a
levigem <levi@trov.com>
2017-05-01 17:38:19 -0700

    update call to database.. still not working

--- jakulebe_code/routes/gameroom.js
+++ jakulebe_code/routes/gameroom.js
@@ -3,7 +3,19 @@ var router = express.Router();
 var {database} = require('../database/database');
 
 router.get('/', function(req, res) {
-  res.render('gameroom');
+
+    let gameRoomName = req.body.gameRoomName;
+    const numberOfPlayers = 4;
+    var insertQuery = `INSERT INTO Games (gameRoomName, max_players) VALUES ($1, $2)`;
+    database.none(insertQuery, [gameRoomName, numberOfPlayers])
+    .then(function()
+    {
+      next();
+    })
+    .catch(function(error){
+      return res.send(error);
+    });
+    res.render('gameroom');
 });
 
 router.post('/createGameRoom', function(req, res, next)
@@ -11,7 +23,7 @@ router.post('/createGameRoom', function(req, res, next)
   let gameRoomName = req.body.gameRoomName;
   const numberOfPlayers = 4;
   var insertQuery = `INSERT INTO Games (gameRoomName, max_players) VALUES ($1, $2)`;
-  client.query(insertQuery, [gameRoomName, numberOfPlayers]);
+  database.query(insertQuery, [gameRoomName, numberOfPlayers]);
 });
 
 module.exports = router;
