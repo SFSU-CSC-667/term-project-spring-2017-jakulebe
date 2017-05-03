3ce50984cced44057d1a6918af85d764326223b2
levigem <levi@trov.com>
2017-05-03 14:49:37 -0700

    show what players are in the game when you open the game room

--- jakulebe_code/routes/game.js
+++ jakulebe_code/routes/game.js
@@ -2,6 +2,29 @@ var express = require('express');
 var router = express.Router();
 var {database} = require('../database/database');
 
+function getPlayersInfo(req, res, next){
+  const gameID = parseInt(req.query.gameID);
+  const getPlayersInGameQuery = `SELECT * FROM registeredUsers WHERE playerID IN (Select playerID FROM Players WHERE gameID = $1)`;
+  const playersInGame = [];
+
+  database.any(getPlayersInGameQuery, [gameID])
+    .then(function(data){
+      if (data != null && data.length > 0)
+      {
+        for (var index = 0; index < data.length; index++)
+        {
+            playersInGame[index] = data[index].username;
+        }
+      }
+      res.locals.playersInGame = playersInGame;
+      next();
+    })
+    .catch(function(error) {
+      console.log("ERROR:",error);
+      return res.send(error);
+      });
+}
+
 
 
 //this is barebones right now, mainly pulling the name of the game from db to display
@@ -23,8 +46,10 @@ router.use(function getGameInfo(req, res, next){
       });
 });
 
+router.use(getPlayersInfo);
+
 router.get('/', function(req, res, next) {
-  res.render('gameroom', {username:req.session.passport.user, gameRoomName: res.locals.gameRoomName});
+  res.render('gameroom', {username:req.session.passport.user, gameRoomName: res.locals.gameRoomName, players: res.locals.playersInGame});
 });
 
 
