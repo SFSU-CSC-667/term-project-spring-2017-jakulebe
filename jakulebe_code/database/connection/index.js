a32ded1aea9517b8b6facc30fc0c9a241f5ef563
levigem <levi@trov.com>
2017-05-06 12:38:11 -0700

    when joining a game, if you are already in the game you do not get added again

--- jakulebe_code/database/connection/index.js
+++ jakulebe_code/database/connection/index.js
@@ -1,4 +1,4 @@
 const pgp = require( 'pg-promise' )()
-const db = pgp( process.env.DATABASE_URL || 'postgres://foxtrot@localhost:5432/jakulebe' )
+const db = pgp( process.env.DATABASE_URL || 'postgres://Levi@localhost:5432/jakulebe' )
 
 module.exports = db
