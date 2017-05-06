ec6084ae763d47d0f8962341275a0339690acbd3
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2017-05-06 01:38:59 -0700

    Create Leaderboard page

---
+++ jakulebe_code/database/users/index.js
@@ -0,0 +1,8 @@
+const db = require('../connection');
+
+const ALL = `SELECT * FROM registeredusers`;
+const WINS_DESC = ALL + " ORDER BY wins DESC, losses ASC"
+module.exports = {
+  all: () => db.any(ALL),
+  wins_desc: () => db.any(WINS_DESC)
+}
