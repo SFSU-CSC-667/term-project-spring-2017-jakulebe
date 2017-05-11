818151c44b5e4f501e83176237af5abcf0e3d0ba
Brett Richard Nitschke <bnitschk@mail.sfsu.edu>
2017-05-11 12:42:14 -0700

    socket stuff working

--- jakulebe_code/public/javascripts/gameRoom.js
+++ jakulebe_code/public/javascripts/gameRoom.js
@@ -3,10 +3,13 @@ const playerID = document.currentScript.getAttribute('playerID');
 const username = document.currentScript.getAttribute('username');
 
 
+const playerChannel = gameID.toString() + username;
+
 const userPackage = new Object();
   userPackage.gameID = gameID;
   userPackage.playerID = playerID;
   userPackage.username = username;
+  userPackage.playerChannel = playerChannel;
 
 
 var socket = io();
@@ -16,4 +19,8 @@ socket.emit('JOIN_GAME', userPackage);
 
 socket.on('TEST', function(userPackage){
   $( '.message' ).text(`${userPackage.username} connected`)
+})
+
+socket.on('PLAYER_TEST', function(userPackage){
+  $( '.playerMessage').text(`this message is for ${userPackage.username} only`);
 })
