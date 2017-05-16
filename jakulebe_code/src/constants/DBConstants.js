46ea32f6147aeba28ac872fe6322ee7ca76fd195
Brett Richard Nitschke <bnitschk@mail.sfsu.edu>
2017-05-16 11:45:52 -0700

    adding a few more constants

--- jakulebe_code/src/constants/DBConstants.js
+++ jakulebe_code/src/constants/DBConstants.js
@@ -1,3 +1,14 @@
+const GET_PLAYER_NAME_BY_ID_QUERY = `SELECT username FROM registeredUsers WHERE player_id = $1`;
+
+const GET_PLAYER_ID_BY_NAME_QUERY = `SELECT player_id FROM registeredUsers WHERE username = $1`;
+
+const GET_PLAYER_NUMBER_BY_ID_QUERY = `SELECT player_number FROM players WHERE player_id = $1
+                                      AND game_id = $2`;
+
+const GET_PLAYER_NUMBER_BY_NAME_QUERY = `SELECT player_number FROM players WHERE username = $1
+                                        AND game_id = $2`;
+
+
 const GAME_FULL_QUERY = `SELECT * FROM Games WHERE game_id = $1`;
 
 const GET_PLAYER_CARDS_BY_PLAYER_ID_QUERY = `SELECT * FROM cards_in_play WHERE game_id = $1 AND player_id = $2`;
@@ -8,21 +19,29 @@ const GO_FISH_QUERY = `UPDATE cards_in_play SET player_id = $1 WHERE card_id IN
 
 const GET_CARDS_LEFT_IN_DECK_QUERY = `SELECT count(*) FROM cards_in_play WHERE game_id = $1 AND player_id = -1`;
 
+//checks to see if 4 of kind exist in hand, returns value
 const FIND_BOOKS_IN_HAND_QUERY = `SELECT value, count(value) FROM cards_in_play WHERE game_id = $1
                                   AND player_id = $2 GROUP BY value HAVING count(value) = 4`;
 
+//if books are found, set player_id of books to -2;
 const MOVE_BOOK_OUT_OF_HAND_QUERY = `UPDATE cards_in_play SET player_id = -2 WHERE card_id IN
                                     (SELECT card_id FROM cards_in_play WHERE game_id = $1
                                     AND player_id = $2 AND value = $3)`;
 
+//Query to see if target player has desired cards
 const CHECK_FOR_REQUESTED_CARDS_QUERY = `SELECT * FROM cards_in_play WHERE player_id = $1
                                         AND game_id = $2 AND value = $3`;
 
+//move cards from $3's hand to $1's hand
 const MOVE_CARDS_TO_REQUESTING_PLAYER_QUERY = `UPDATE cards_in_play SET player_id = $1 WHERE
                                               card_id IN (SELECT card_id from cards_in_play WHERE
                                               game_id = $2 AND player_id = $3 AND value = $4)`;
 
-module.exports = {GAME_FULL_QUERY,
+module.exports = {GET_PLAYER_NAME_BY_ID_QUERY,
+                  GET_PLAYER_ID_BY_NAME_QUERY,
+                  GET_PLAYER_NUMBER_BY_ID_QUERY,
+                  GET_PLAYER_NUMBER_BY_NAME_QUERY,
+                  GAME_FULL_QUERY,
                   GET_PLAYER_CARDS_BY_PLAYER_ID_QUERY,
                   GO_FISH_QUERY,
                   GET_CARDS_LEFT_IN_DECK_QUERY,
