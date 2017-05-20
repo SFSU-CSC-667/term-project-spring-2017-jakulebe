9fa9c2603e0edab0a335d445ba5d1e459e33740c
levigem <levi@trov.com>
2017-05-20 03:13:44 -0700

    adding database creds

--- database/connection/index.js
+++ database/connection/index.js
@@ -1,5 +1,5 @@
 const pgp = require( 'pg-promise' )()
-const userName = require( 'userinfo' ).whoami();
-const db = pgp( process.env.DATABASE_URL || `postgres://${userName}@localhost:5432/jakulebe` )
+//const userName = require( 'userinfo' ).whoami();
+const db = pgp( process.env.DATABASE_URL || `postgres://$lgemmell@mail.sfsu.edu:jakulebe1@localhost:5432/jakulebe` )
 
 module.exports = db
